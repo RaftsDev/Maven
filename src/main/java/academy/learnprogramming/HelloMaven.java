ca887e48f7f17b18363a65f8a865ec6e8405f48e
Ralph <raftsdev@gmail.com>
2022-07-20 20:53:42 -0400

    Chore: Check.

--- src/main/java/academy/learnprogramming/HelloMaven.java
+++ src/main/java/academy/learnprogramming/HelloMaven.java
@@ -1,11 +1,9 @@
 package academy.learnprogramming;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HelloMaven {
-
     private final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
     public static void main(String[] args) {
        log.info("Hello Maven!");
